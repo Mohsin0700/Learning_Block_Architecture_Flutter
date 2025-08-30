a2434dede5b2d87a2f85917c9447b55edceacd3f
Mohsin0700 <muhammad786mohsin024@gmail.com>
2025-08-31 04:47:44 +0500

    Files are updated

--- lib/mini_apps/counter_app/counter_cubit.dart
+++ lib/mini_apps/counter_app/counter_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 
 class CounterCubit extends Cubit<int> {
+  int currentState = 0;
   CounterCubit() : super(0);
 
   void increment() {
@@ -12,6 +13,7 @@ class CounterCubit extends Cubit<int> {
   void onChange(Change<int> change) {
     super.onChange(change);
     print('Change from ${change.currentState} to ${change.nextState}');
+    currentState = change.currentState;
   }
 
   @override
--- lib/mini_apps/counter_app/counter_view.dart
+++ lib/mini_apps/counter_app/counter_view.dart
@@ -1,18 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learning_block/mini_apps/counter_app/counter_cubit.dart';
 
-class CounterApp extends StatelessWidget {
-  CounterApp({super.key});
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
   final counterCubit = CounterCubit();
+
+  @override
+  void dispose() {
+    super.dispose();
+    counterCubit.close();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Counter App')),
+      appBar: AppBar(
+        title: Text('Counter App'),
+        backgroundColor: Colors.lime,
+        actions: [
+          BlocBuilder<CounterCubit, int>(
+            bloc: counterCubit,
+            builder: (context, count) => Container(
+              margin: const EdgeInsets.only(right: 10),
+              child: Badge.count(
+                count: count,
+                child: Icon(Icons.shopping_bag_outlined),
+              ),
+            ),
+          ),
+        ],
+      ),
       body: Center(
-        child: Text(
-          counterCubit.state.toString(),
-          style: TextStyle(fontSize: 100),
+        child: BlocBuilder<CounterCubit, int>(
+          bloc: counterCubit,
+          builder: (context, count) {
+            return Column(
+              children: [
+                Text('Changing From:', style: TextStyle(fontSize: 50)),
+                Text(
+                  counterCubit.currentState.toString(),
+                  style: TextStyle(fontSize: 100),
+                ),
+                Text('To', style: TextStyle(fontSize: 50)),
+                Text(
+                  counterCubit.state.toString(),
+                  style: TextStyle(fontSize: 100),
+                ),
+              ],
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
